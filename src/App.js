dffbcbe8e54fa55b2dfbf7936922b845184e94d8
mohsin-8 <mohsinalikhan518@gmail.com>
2022-05-02 23:09:52 +0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <>
+      <h1>Panacloud Bootcamp2020</h1>
+      <h2>Session One | Github Actions</h2>
+      <h3>Developer: Mohsin Ali Khan</h3>
+    </>
+  )
+}
+
+export default App;
