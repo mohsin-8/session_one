8713addc9c1c86bcc01681fd5ae50129a24a44bf
mohsin-8 <mohsinalikhan518@gmail.com>
2022-05-03 16:08:57 +0500

    added on App.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ const App = () => {
     <>
       <h1>Panacloud Bootcamp2020</h1>
       <h2>Session One | Github Actions</h2>
+      <h3>Auto Deployment</h3>
       <h3>Developer: Mohsin Ali Khan</h3>
     </>
   )
